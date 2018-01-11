e46bf8941a162ab4e3ec41b36c47556ad97a02dc
Florian Konnertz <fkonnertz@posteo.de>
2018-01-11 09:24:42 +0100

    gpio pins in model and more

--- sensors/models.py
+++ sensors/models.py
@@ -10,6 +10,7 @@ class Sensor(models.Model):
     name    = models.CharField(max_length=16)
     descr   = models.CharField(max_length=255)
     unit    = models.CharField(max_length=16)
+    pin_bcm = models.IntegerField()
     
 
 
@@ -17,5 +18,6 @@ class Measurement(models.Model):
         
     sensor  = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     dtime   = models.DateTimeField('date published')
-    value   = models.DecimalField(max_digits=4, decimal_places=2)
-    
+    temperature = models.DecimalField(max_digits=4, decimal_places=2)
+    resistance  = models.IntegerField()
+
