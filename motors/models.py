f5c24aa4fa29efd2cc673b822095bf12c488bc46
Florian Konnertz <fkonnertz@posteo.de>
2018-01-12 13:05:57 +0100

    motor models and start templates

--- motors/models.py
+++ motors/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Motor(models.Model):
+    
+    name    = models.CharField(max_length=16)
+    descr   = models.CharField(max_length=255)
+    last_toggle = models.CharField(max_length=16)
+    
+
+
+class Toggle(models.Model):
+        
+    toggle = models.ForeignKey(Motor, on_delete=models.CASCADE)
+    dtime   = models.DateTimeField('date published')
+    state   = models.BooleanField('Status')
--- motors/views.py
+++ motors/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
-# Create your views here.
+from .models import Motor
+
+
+
+def index(request):
+
+    motors = Motor.objects.all()
+    c = RequestContext(request)
+    c['entries'] = motors
+    return render(request, 'index.html')
+
+
