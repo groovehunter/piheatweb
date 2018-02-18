b26ef4164ca5723fd2dc346d69a8177d7e70dca5
Florian Konnertz <fkonnertz@posteo.de>
2018-02-19 00:50:36 +0100

    migrations for rules

--- motors/models.py
+++ motors/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 
 
+class Rule(models.Model):
+
+    name = models.CharField(max_length=32)
+    descr= models.CharField(max_length=255)
+    logic= models.CharField(max_length=255)
+    count= models.IntegerField()
+
 class Motor(models.Model):
     
     name    = models.CharField(max_length=16)
@@ -15,3 +22,10 @@ class Toggle(models.Model):
     toggle = models.ForeignKey(Motor, on_delete=models.CASCADE)
     dtime   = models.DateTimeField('date published')
     state   = models.BooleanField('Status')
+    rule    = models.ForeignKey(Rule, on_delete=models.CASCADE)
+
+
+
+
+
+
