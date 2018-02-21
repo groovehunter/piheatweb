df40c9232a8f9bb6812332b2032ba84023c34e49
Florian Konnertz <fkonnertz@posteo.de>
2018-02-21 18:16:21 +0100

    rule ref in toggle, and rule history, rework migrations

--- motors/models.py
+++ motors/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
+# default Rule
+DEFAULT_RULE = 1
+DEFAULT_MOTOR= 1
+DEFAULT_TOGGLE_RULE = 2
 
 
 class Rule(models.Model):
@@ -7,7 +11,19 @@ class Rule(models.Model):
     name = models.CharField(max_length=32)
     descr= models.CharField(max_length=255)
     logic= models.CharField(max_length=255)
-    count= models.IntegerField()
+    count= models.PositiveIntegerField()
+
+
+class RuleHistory(models.Model):
+
+    rule_id = models.ForeignKey(Rule, on_delete=models.CASCADE, default=DEFAULT_RULE)
+    name = models.CharField(max_length=32)
+    descr= models.CharField(max_length=255)
+    logic= models.CharField(max_length=255)
+    count= models.PositiveIntegerField()
+
+
+
 
 class Motor(models.Model):
     
@@ -19,13 +35,9 @@ class Motor(models.Model):
 
 class Toggle(models.Model):
         
-    toggle = models.ForeignKey(Motor, on_delete=models.CASCADE)
+    toggle = models.ForeignKey(Motor, on_delete=models.CASCADE, default=DEFAULT_MOTOR)
     dtime   = models.DateTimeField('date published')
     state   = models.BooleanField('Status')
-    rule    = models.ForeignKey(Rule, on_delete=models.CASCADE)
-
-
-
-
+    rule    = models.ForeignKey(Rule, on_delete=models.CASCADE, default=DEFAULT_TOGGLE_RULE)
 
 
