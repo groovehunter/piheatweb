5087fefe363d018647e188a56a1d92f1e067d517
Florian Konnertz <fkonnertz@posteo.de>
2021-01-01 15:50:31 +0100

    each sensor its own table

--- sensors/models.py
+++ sensors/models.py
@@ -1,23 +1,38 @@
 from django.db import models
 
 
-class Sensor(models.Model):
-    
+class SensorBase(models.Model): 
     UNIT_AVAIL = (
         (u'\xb0C',  'degree Celsius'),
         ('F',       'Fahrenheit'),
     )
+
+
+class SensorInfo(models.Model):
     name    = models.CharField(max_length=16)
     descr   = models.CharField(max_length=255)
     unit    = models.CharField(max_length=16)
     pin_bcm = models.IntegerField()
     
 
-
-class Measurement(models.Model):
-        
-    sensor  = models.ForeignKey(Sensor, on_delete=models.CASCADE)
-    dtime   = models.DateTimeField('date published')
+class SensorData_01(SensorBase):
+    dtime   = models.DateTimeField('date of measurement')
     temperature = models.DecimalField(max_digits=4, decimal_places=2)
     resistance  = models.IntegerField()
+    
+class SensorData_02(SensorBase):
+    dtime   = models.DateTimeField('date of measurement')
+    temperature = models.DecimalField(max_digits=4, decimal_places=2)
+    resistance  = models.IntegerField()
+    
+class SensorData_03(SensorBase):
+    dtime   = models.DateTimeField('date of measurement')
+    temperature = models.DecimalField(max_digits=4, decimal_places=2)
+    resistance  = models.IntegerField()
+    
+class SensorData_04(SensorBase):
+    dtime   = models.DateTimeField('date of measurement')
+    temperature = models.DecimalField(max_digits=4, decimal_places=2)
+    resistance  = models.IntegerField()
+    
 
