887cc51e6af5bbf17bbcdde8b011e50fa72b91fc
Florian Konnertz <fkonnertz@posteo.de>
2021-01-02 22:27:37 +0100

    dtime.minute=0 in query

--- sensors/views.py
+++ sensors/views.py
@@ -72,8 +72,11 @@ class SensorDataView(Controller):
         self.context['sensorinfo'] = sensorinfo
         modelname = eval('SensorData_'+'0'+str(sid))
         self.init_ctrl()
-        object_list = eval('modelname.objects.filter(dtime__minute=0).order_by("-dtime")')
-        #object_list = SensorData_01.objects.filter(dtime__minute=0).order_by("-dtime")
+        start_date = datetime.date(2021, 1, 2)
+        end_date = datetime.date(2021, 1, 3)
+        #object_list = eval('modelname.objects.filter(dtime__minute=0).order_by("-dtime")')
+        object_list = SensorData_01.objects.filter(dtime__minute=0).filter(dtime__range=(start_date, end_date))
+
 #        object_list = SensorData_01.objects.filter(resistance__gt=6500).order_by("-dtime")
 #        object_list = SensorData_01.objects.dates('dtime', 'day')
         self.lg.debug(len(object_list))
@@ -83,8 +86,8 @@ class SensorDataView(Controller):
         self.context['table'] = table
 
         self.template = 'sensors/data.html'
-        self.context['object_list'] = object_list
-        self.context['data'] = object_list
+        #self.context['object_list'] = object_list
+        #self.context['data'] = object_list
         keys = ['dtime', 'temp', 'resistance']
         self.context['keys'] = keys
         return self.render()
