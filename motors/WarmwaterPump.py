1e2b59043d91c25b70565c083c05532d75ab45f7
Florian Konnertz <fkonnertz@posteo.de>
2021-01-07 14:10:59 +0100

    just initial version

---
+++ motors/WarmwaterPump.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+import RPi.GPIO as GPIO
+import time
+import sys
+
+
+
+class WarmwaterPumCtrl(object):
+
+    def __init__(self):
+        self.count = 0
+
+    def setup(self):
+        GPIO.setmode(GPIO.BCM)
+        self.pins = {}
+        self.pins['relay'] = 0
+        self.pins['sth'] = 0
+        GPIO.setwarnings(False)
+        for name, pin in self.pins.items():
+            GPIO.setup(pin, GPIO.OUT)
+
+
+
+    def enable(self):
+        PUL = self.pins['PUL']
+        GPIO.output(PUL, GPIO.LOW)
+        time.sleep(sl)
+
+    def disable(self):
+        PUL = self.pins['PUL']
+        GPIO.output(PUL, GPIO.LOW)
+        time.sleep(sl)
+
+
+    def cleanup(self):
+        pass 
+
+
