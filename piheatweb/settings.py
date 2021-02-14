c22bdbe0b13226731b62603ddc44e7634472fddc
Florian Konnertz <fkonnertz@posteo.de>
2021-02-15 00:32:09 +0100

    logging in django

--- piheatweb/settings.py
+++ piheatweb/settings.py
@@ -102,3 +102,22 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"), )
 #STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': '/home/pi/log/debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
