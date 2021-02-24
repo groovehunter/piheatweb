abf47fec51a75b89c000abd27e34b45f1f61c9c9
kRa554C064 <fkonnertz@posteo.de>
2021-02-24 11:36:13 +0100

    add ref to self as rule attr

--- motors/RulesCtrl.py
+++ motors/RulesCtrl.py
@@ -3,12 +3,41 @@ from motors.Rules import *
 import motors.Rules
 
 from motors import rules
-from motors.models import Rule, RuleHistory
+from motors.models import Rule, RuleHistory, RuleResultHistory
 from motors.KlassLoader import KlassLoader
 
 logger = logging.getLogger()
 
-class RulesCliCtrl(KlassLoader):
+class Calc:
+  def load_sensordata(self):
+    some = SensorData_04.objects.order_by('-dtime')[1:30]
+    cur = some.aggregate(Avg('temperature'))['temperature__avg']
+    self.cur_outdoor = float(cur)
+
+  def vorlauf_soll_temp(self):
+    logger.debug("current outdoor temp %s", self.cur_outdoor)
+    logger.debug("calculated Soll by outdoor temp %s", self.soll_calc)
+
+    ### nachtabsenkung
+    absenk = 0
+    now = datetime.now()
+    if (now.hour > 23 or now.hour < 5):
+      absenk = -7
+    self.soll_calc = self.soll_calc + absenk
+    logger.debug('nachtabsenkung?: %s', absenk)
+    logger.debug("calculated final Soll:  %s", self.soll_calc)
+
+    # Is Soll to Ist difference more than 2
+    if (abs(self.logic - self.soll_calc) > 2):
+      logic_new = str(round(self.soll_calc))
+      logger.debug("ACT: setting rule logic to %s", logic_new)
+      self.rule.logic = logic_new
+      self.rule.save()
+    else:
+      logger.debug('OK - leave rule logic as is: %s', self.rule.logic)
+
+
+class RulesCliCtrl(KlassLoader, Calc):
   """ non web controller """
   def setup(self):
     self.klass_list = self.get_klasslist(motors.Rules)
@@ -40,7 +69,7 @@ class RulesCliCtrl(KlassLoader):
   def check_rule(self, rule):
     rule_klass_obj = self.klass_obj_list[rule.name]
     rule_klass_obj.set_rule(rule)
-
+    rule_klass_obj.ctrl = self
     rule_klass_obj.setup()
     rule_klass_obj.report()
     if not rule_klass_obj.check():
@@ -63,5 +92,3 @@ class RulesCliCtrl(KlassLoader):
     rule_klass_obj.report()
     if not rule_klass_obj.check():
       rule_klass_obj.action()
-
-
