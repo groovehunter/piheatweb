ff2d79d73459a1752bcdc3c1384c9233f555ba27
kRa554C064 <fkonnertz@posteo.de>
2022-10-27 20:43:54 +0200

    reader_daemon

--- reader_daemon.py
+++ reader_daemon.py
@@ -20,34 +20,24 @@ adc = Adafruit_ADS1x15.ADS1115()
 logging.getLogger("Adafruit_I2C").setLevel(logging.WARNING)
 
 def read_adc():
-  from django.utils import timezone 
+  from django.utils import timezone
   # Read all the ADC channel values in a list.
   values = [0]*4
   tz_now = timezone.now() # TZ aware :)
 
-  event = ReadingEvent(dtime=tz_now)
-  #ctrl_event = ControlEvent(dtime=tz_now)
-  #ctrl_event.save()
   ctrl_event = ControlEvent.objects.latest('dtime')
 
   for i in range(4):
     values[i] = adc.read_adc(i, gain=GAIN, data_rate=128)
     sid = '0'+str(i+1)
-    
+
     obj = eval('SensorData_'+sid+'()')
-    obj.dtime = tz_now
     obj.adc_out = values[i]
     obj.temperature = 0
     obj.resistance = 0
     obj.ctrl_event = ctrl_event
     obj.save()
-    #logger.info(obj.ctrl_event.id)
-    #logger.info(obj.ctrl_event_id)
-    evalstr = 'event.sid'+sid+'=obj'
-    exec(evalstr)
- 
-  event.save()
-
+    logger.debug(obj.ctrl_event.id)
 
 
 # cron version
@@ -55,6 +45,3 @@ from calc_temp_latest_daemon import tempcalc
 if __name__ == '__main__':
   read_adc()
   tempcalc()
-
-
-
